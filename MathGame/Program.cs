080b19e2724140d6405e65446e53d20d3c4f6291
Mansor Almossa <malmossa@gmail.com>
2023-06-02 10:05:54 -0700

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Please type your name");
+
+string name = Console.ReadLine();
+DateTime date = DateTime.Now;
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+                   A - Addition.
+                   S - Subtraction.
+                   M - Multiplication.
+                   D - Division.
+                   Q - Quit the program.");
+Console.WriteLine("---------------------------------");
+
+string gameSelected = Console.ReadLine();
+
+if(gameSelected.Trim().ToLower() == "a")
+{
+    AdditionGame("Addition game selected");
+} if(gameSelected.Trim().ToLower() == "s")
+{
+    SubtractionGame("Subtraction game is selected");
+} if (gameSelected.Trim().ToLower() == "m")
+{
+    Multiplication("Multiplication game is selected");
+} if(gameSelected.Trim().ToLower() == "d")
+{
+    Division("Division game is selected");
+} if(gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye!");
+}
+
+
+void AdditionGame(string message) 
+{
+    Console.WriteLine(message);
+}
+
+void SubtractionGame(string message) 
+{ 
+    Console.WriteLine(message);
+}
+
+void Multiplication(string message) 
+{
+    Console.WriteLine(message);
+}
+
+void Division(string message)
+{
+    Console.WriteLine(message);
+}
+
+
+
+Console.ReadLine();
+
+
