be98b2fe989ce03f566f096a94cad2c3c66ba86c
Mansor Almossa <malmossa@gmail.com>
2023-06-12 08:37:32 -0700

    completed

--- MathGame/Helpers.cs
+++ MathGame/Helpers.cs
@@ -4,9 +4,21 @@ namespace MathGame
 {
     internal class Helpers
     {
-        internal static List<Game> games = new List<Game>();
+        internal static List<Game> games = new List<Game>
+        {
+            new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
+            new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
+            new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
+            new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
+            new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
+            new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
+            new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
+            new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
+        };
         internal static void PrintGames()
         {
+            var gamesTpPrint = games.Where(x => x.Date > new DateTime(2023, 05, 09)).OrderByDescending(x => x.Score);
+
             Console.Clear();
             Console.WriteLine("Games History");
             Console.WriteLine("-------------------");
